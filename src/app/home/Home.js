3293a9e0d994571689e8a779f262df5784571d61
Juan Viveros <jviverosd1@gmail.com>
2023-03-02 07:10:52 -0500

    a

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -31,7 +31,7 @@ const Home = () => {
       <svg
         viewBox="0 0 200 200"
         xmlns="http://www.w3.org/2000/svg"
-        className="absolute -top-80 -right-96"
+        className="absolute -top-80 -right-96 -z-10"
         height=""
       >
         <path
