db3b5feae5a09d72c718d78a79838610a15b21cf
Juan Viveros <jviverosd1@gmail.com>
2023-03-02 00:03:36 -0500

    a

--- src/app/sites/components/Map.js
+++ src/app/sites/components/Map.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useNavigate } from "react-router-dom";
-import { Dialog } from "@mui/material";
-import { MusicalNoteIcon } from "@heroicons/react/24/solid";
-
+import L from "leaflet";
+import { PlayCircleIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
 
 const Map = () => {
   const navigate = useNavigate();
@@ -16,36 +16,52 @@ const Map = () => {
         "Área boscosa pequeña con restaurantes, bares y una emblemática escultura de perro que se ilumina en la noche.",
       lat: 3.4358476,
       lng: -76.5477402,
+      url: "parque-del-perro",
     },
     {
       id: 2,
       name: "Pance",
-      description: "Parque de la música",
+      description:
+        "Pance es un corregimiento en el sur del municipio colombiano de Cali, su área se extiende desde el Valle del río Cauca hasta el parque nacional natural los Farallones de Cali.",
       lat: 3.3492978,
       lng: -76.5736431,
+      url: "pance",
     },
     {
       id: 3,
       name: "Plazoleta Jairo Varela",
-      description: "Parque de la música",
+      description:
+        "La Plazoleta Jairo Varela está situada en el centro histórico de Cali, cerca de la iglesia de San Francisco y la iglesia de La Merced. Es un lugar de reunión popular tanto para los lugareños como para los turistas, y se utiliza a menudo como sede de eventos culturales y conciertos.",
       lat: 3.4550312,
       lng: -76.5370291,
+      url: "plazoleta-jairo-varela",
     },
     {
       id: 4,
       name: "La Ermita",
-      description: "Parque de la música",
+      description:
+        "Ermita es un barrio ubicado en la ciudad de Cali, Colombia. Es conocido por sus coloridas casas coloniales y su ambiente bohemio y artístico. El barrio cuenta con numerosos restaurantes, bares y galerías de arte, y es un destino popular para los turistas y los lugareños que buscan una experiencia cultural única.",
       lat: 3.4546141,
       lng: -76.5349964,
+      url: "la-ermita",
     },
     {
       id: 5,
       name: "San Antonio",
-      description: "Parque de la música",
+      description:
+        "San Antonio es un conocido barrio del occidente de la ciudad colombiana de Cali. San Antonio es famoso y reconocido como un barrio tradicional, por su arquitectura colonial y su historia.",
       lat: 3.4477814,
       lng: -76.5425425,
+      url: "san-antonio",
     },
   ];
+
+  const MusicalNoteIcon = new L.Icon({
+    iconUrl: require("./assets/musica.gif"),
+    iconRetinaUrl: require("./assets/musica.gif"),
+    iconSize: new L.Point(30, 40),
+  });
+
   return (
     <>
       <MapContainer center={position} zoom={12}>
@@ -57,9 +73,26 @@ const Map = () => {
           <Marker
             key={site.id}
             position={[site.lat, site.lng]}
-            onChange={() => navigate("")}
+            icon={MusicalNoteIcon}
           >
-            
+            <Popup>
+              <div className="text-white font-bold text-2xl text-center bg-yellow-400 p-3 rounded-2xl cursor-default">
+                <h5 className="p-4 bg-orange-500 rounded-lg">{site.name}</h5>
+              </div>
+              <div className="flex items-center">
+                <div>
+                  <p className="text-sm">
+                    <span className="font-semibold">Descripción:</span> <br />
+                    {site.description}
+                  </p>
+                </div>
+                <div className="cursor-pointer">
+                  <Link to="/sites">
+                    <PlayCircleIcon className="text-yellow-400 h-20 w-20 rounded-full bg-orange-500 shadow-2xl" />
+                  </Link>
+                </div>
+              </div>
+            </Popup>
           </Marker>
         ))}
       </MapContainer>
